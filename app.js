12f2e40b47e8d6afd82b0911710042cccf8306d8
Stephen Thilenius <cmosdsnr@gmail.com>
2022-09-05 16:02:31 -0700

    s

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ app.listen(port, () => {
 })
 
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true, limit: '10mb' }))
+app.use(express.json({ limit: '10mb' }))
 app.use(express.static('./public'))
 
 
