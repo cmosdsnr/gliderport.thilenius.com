d855047dd5bfab098037f3500e19ec08b50a637f
Stephen Thilenius <cmosdsnr@gmail.com>
2022-09-07 12:40:20 -0700

    s

--- app.js
+++ app.js
@@ -153,7 +153,7 @@ app.post("/addData", (req, res) => {
             connection.query(sql, (err, results, fields) => { })
 
             //let's work on hours Db
-            const dtd = Date.now() / 1000 + 60 * Date.getTimezoneOffset()
+            const dtd = Date.now() / 1000
             const thisHour = 3600 * parseInt(dtd / 3600);
             const twoDaysAgo = thisHour - 48 * 3600;
 
@@ -172,7 +172,9 @@ app.post("/addData", (req, res) => {
                     var dt1 = new Date(i * 1000)
                     var dt2 = new Date((3600 + i) * 1000)
                     // console.log(i + " " + dt1.toISOString() + " " + latestHours + " " + twoDaysAgo)
-                    sql = "SELECT * FROM `gliderport` WHERE recorded > '" + dt1.toISOString() + "' AND recorded <= '" + dt2.toISOString() + "'";
+                    sql = "SELECT * FROM `gliderport` WHERE recorded > '" +
+                        dt1.toLocaleString('en-US', { timeZone: 'America/Los_Angeles' }) + "' AND recorded <= '" +
+                        dt2.toLocaleString('en-US', { timeZone: 'America/Los_Angeles' }) + "'";
                     connection.query(sql, (err, results, fields) => {
                         results?.forEach((v, j) => {
                             data.date.push((new Date(v.recorded)).getTime() / 1000 - i);
