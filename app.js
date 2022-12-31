ff311a41b2501fd09168a76ab5495a66fd64149e
Stephen Thilenius <cmosdsnr@gmail.com>
2022-12-30 22:11:24 -0800

    wip

--- app.js
+++ app.js
@@ -487,8 +487,12 @@ app.post("/addData", (req, res) => {
                         if (Array.isArray(results)) {
                             console.log("   Since the last record in code_history at ", (new Date(tsLast * 1000)).toISOString(), " with code ",
                                 lc, ", there are ", results.length, " new data points in gliderport")
+                            let x = 0
                             results.forEach((v, i) => {
-                                if (i % 1000 === 0) console.log("x")
+                                if (r.date != x) {
+                                    console.log(r.date)
+                                    x = r.date
+                                }
                                 const ts = Math.round((new Date(v.recorded)).getTime / 1000)
                                 if (ts > r.date + r.data.sun[0]) {
                                     // after sunrise
