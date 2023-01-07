95e8d4a74f4ad806dacdb500ba6d576101dd841e
Stephen Thilenius <cmosdsnr@gmail.com>
2023-01-07 06:47:47 -0800

    wip

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -3,7 +3,7 @@ import JSMpeg from 'jsmpeg-player'
 
 export default function Video() {
     useEffect(() => {
-        let videoUrl = 'ws://gliderportvideo.thilenius.org'
+        let videoUrl = 'wss://gliderportvideo.thilenius.org'
         // let videoUrl = 'ws://localhost:8081'
         let videoWrapper = document.getElementById("videoWrapper");
         let player = new JSMpeg.VideoElement(videoWrapper, videoUrl)
