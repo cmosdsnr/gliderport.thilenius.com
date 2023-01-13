3b5e779487ccc1a0cfd43e4d04fd7a989e97b346
Stephen Thilenius <cmosdsnr@gmail.com>
2023-01-13 07:46:09 -0800

    wip

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -2,7 +2,8 @@ import React, { useRef, useState, useEffect } from 'react'
 import { useData } from '../contexts/DataContext'
 import "../css/forecast.css"
 export default function Forecast() {
-
+    const [headers, setHeaders] = useState([])
+    const [rows, setRows] = useState([[]])
     const { loadData, forecastFull } = useData()
 
     // load history
@@ -12,6 +13,36 @@ export default function Forecast() {
 
     useEffect(() => {
         console.log(forecastFull)
+        if (forecastFull?.length > 0) {
+            let keysList = {}
+            forecastFull.forEach((v, i) => {
+                keysList = { ...keysList, ...v }
+            })
+            let keys = Object.keys(keysList)
+            setHeaders(keys)
+            let hr = []
+            let hrs = []
+            forecastFull.forEach((v, i) => {
+                keys.forEach((w, j) => {
+                    if (v[w] != null) {
+                        if (v[w]['1h'] != null)
+                            hr.push(v[w]['1h'])
+                        else if (w === 'dt') {
+                            const dt = new Date(1000 * v[w])
+                            hr.push(dt.toLocaleString().replace(/\/[0-9]*,/, ""))
+                        } else if (w === "temp" || w === "feels_like" || w === "dew_point") {
+                            hr.push(parseInt(v[w]))
+                        }
+                        else
+                            hr.push(v[w])
+                    }
+                    else hr.push("")
+                })
+                hrs.push(hr)
+                hr = []
+            })
+            setRows(hrs)
+        }
     }, [forecastFull])
 
 
@@ -19,18 +50,21 @@ export default function Forecast() {
         <>
             <h1><center>Forecast Data for the next 48 hrs</center></h1>
             <table style={{ marginRight: "50px" }}>
-                <tr>
-                    {Object.keys(forecastFull[0])?.map((d, i) => {
-                        return (<th>{d}</th>)
+                <tbody>
+                    <tr>
+                        {headers.map((d, i) => {
+                            return (<th key={i}>{d}</th>)
+                        })}
+                    </tr>
 
+                    {rows.map((row, i) => {
+                        return (<tr key={i}>
+                            {Array.isArray(row) ? row.map((w, j) => {
+                                return (<td key={j}>{w}</td>)
+                            }) : <></>}
+                        </tr>)
                     })}
-                </tr>
-
-                {forecastFull?.map((d, i) => {
-                    return (<tr>
-                        {Object.keys(d).map((w, j) => { return (<td>{JSON.stringify(d[w])}</td>) })}
-                    </tr>)
-                })}
+                </tbody>
             </table>
         </>
     )
