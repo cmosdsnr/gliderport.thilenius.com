ab55ad31974755ab99aefe3d0e05c16bae3fcb47
Stephen Thilenius <cmosdsnr@gmail.com>
2023-01-25 07:30:54 -0800

    wip

--- app.js
+++ app.js
@@ -636,7 +636,6 @@ app.post("/addData", async (req, res) => {
                     let lastCode = -1
                     debugInfo.openWeather = {}
                     debugInfo.openWeather.hours = responseJson.hourly.length
-                    debugInfo.openWeather.ts = tsNow
                     debugInfo.openWeather.start = responseJson.hourly[0].dt
                     debugInfo.openWeather.stop = responseJson.hourly[responseJson.hourly.length - 1].dt
                     // console.log(`found ${responseJson.hourly.length} hours in forecast, starting at ${timestampToString(responseJson.hourly[0].dt + offset / 1000)} ending ${timestampToString(responseJson.hourly[responseJson.hourly.length - 1].dt + offset / 1000)}`)
@@ -979,9 +978,10 @@ app.get("/info", async (req, res) => {
         content += `Hour in hours table starts at ${v} had ${timestampToString(hourInfo.start)} rows and now has ${hourInfo.l} rows<br/>`
     })
     content += `</p><p>Forecast updating<br/>`
+    content += `Next forecast update as recorded in server_sent: ${timestampToString(debugInfo.tsLast + offset + 3600 * 60)}<br/><br/>`
     content += `Last forecast update as recorded in server_sent: ${timestampToString(debugInfo.tsLast + offset)}<br/>`
-    content += `updating forecast at: ${timestampToString(debugInfo.openWeather.ts + offset)} (${debugInfo.openWeather.ts})<br/>`
     content += `found ${debugInfo.openWeather.hours} hours in forecast, starting at ${timestampToString(debugInfo.openWeather.start + offset / 1000)} ending ${timestampToString(debugInfo.openWeather.stop + offset / 1000)}<br/>`
+
     content += `</p><p>Code history updating<br/>`
     content += `Last update : ${timestampToString(debugInfo.codeHistory.date)} (${debugInfo.codeHistory.date})<br/>`
     content += `Since the last record in code_history at ${timestampToString(debugInfo.codeHistory.tsLast)} with code ${debugInfo.codeHistory.code} there are ${debugInfo.codeHistory.gpResults} new data points in gliderport<br/>`
