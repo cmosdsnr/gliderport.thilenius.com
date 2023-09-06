908a76e4cd85948a04c2505d9d57ba0b499672af
Stephen Thilenius <cmosdsnr@gmail.com>
2023-09-06 10:22:29 -0700

    wip

--- src/firebase.js
+++ src/firebase.js
@@ -14,13 +14,13 @@ dotenv.config()
 //     measurementId: process.env.FIREBASE_API_KEY
 // })
 const app = initializeApp({
-    apiKey: "AIzaSyDPOOdfatZIzOZe7RT - s_aUnG11sste6xk",
-    authDomain: "netninja - game - guidez - 1d8c2.firebaseapp.com",
-    projectId: "netninja - game - guidez - 1d8c2",
-    storageBucket: "netninja - game - guidez - 1d8c2.appspot.com",
+    apiKey: "AIzaSyDPOOdfatZIzOZe7RT-s_aUnG11sste6xk",
+    authDomain: "netninja-game-guidez-1d8c2.firebaseapp.com",
+    projectId: "netninja-game-guidez-1d8c2",
+    storageBucket: "netninja-game-guidez-1d8c2.appspot.com",
     messagingSenderId: "553078821803",
-    appId: "1: 553078821803: web: 28eb58796f56fc4ab8f8ee",
-    measurementId: "G - CM7SJ8PMS0"
+    appId: "1:553078821803:web:28eb58796f56fc4ab8f8ee",
+    measurementId: "G-CM7SJ8PMS0"
 })
 
 export const auth = getAuth(app);
