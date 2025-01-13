8568e713082751a242b470e897830ac38c660c57
Stephen <stephen@thilenius.com>
2025-01-13 06:43:53 -0800

    wip

--- src/components/Diagnostics.tsx
+++ src/components/Diagnostics.tsx
@@ -21,18 +21,24 @@ const Diagnostics: React.FC = () => {
                     <thead>
                         <tr>
                             <th style={{ border: '1px solid black', padding: '8px' }}>Client No</th>
-                            {clients.length > 0 && Object.keys(clients[0]).map((key, j) => (
-                                <th key={j} style={{ border: '1px solid black', padding: '8px' }}>{key}</th>
-                            ))}
+                            {clients.length > 0 && Object.keys(clients[0]).map((key, j) => {
+                                if (key != "updateClients")
+                                    return (<th key={j} style={{ border: '1px solid black', padding: '8px' }}>{key}</th>)
+                                else return null;
+                            }
+                            )}
                         </tr>
                     </thead>
                     <tbody>
                         {clients.map((client, i) => (
                             <tr key={i}>
                                 <td style={{ border: '1px solid black', padding: '8px' }}>{i + 1}</td>
-                                {Object.keys(client).map((key, j) => (
-                                    <td key={j} style={{ border: '1px solid black', padding: '8px' }}>{client[key as keyof typeof client]}</td>
-                                ))}
+                                {Object.keys(client).map((key, j) => {
+                                    if (key != "updateClients")
+                                        return (<td key={j} style={{ border: '1px solid black', padding: '8px' }}>{client[key as keyof typeof client]}</td>)
+                                    else return null;
+                                }
+                                )}
                             </tr>
                         ))}
                     </tbody>
