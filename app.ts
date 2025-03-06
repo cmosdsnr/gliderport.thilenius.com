08979aa1ee7d45c11e802864927d6f189c0c7a6c
Stephen <stephen@thilenius.com>
2025-03-05 20:29:34 -0800

    wip

--- app.ts
+++ app.ts
@@ -53,6 +53,105 @@ const ToId = (x: string) => {
   return "0".repeat(15 - x.length) + x;
 };
 
+function isDirectory(path: string): boolean {
+  try {
+    return fs.statSync(path).isDirectory();
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+}
+
+function getFileDate(filePath: string): Date | null {
+  try {
+    const stats = fs.statSync(filePath);
+    return stats.mtime;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+}
+
+interface ImageStats {
+  earliestFile: string;
+  earliestTime?: number;
+  isContinuous: boolean;
+  formatType: number; // 0:image1000.jpg 1:image10000.jpg 2:image-1/2-10000.jpg
+  lastFile: string;
+  lastTime?: number;
+  numFiles: number;
+  numMissing: number;
+  error?: string;
+  smallestIndex: number;
+  largestIndex: number;
+}
+
+function getImageStats(directoryPath: string): ImageStats {
+  let index: boolean[] = Array(9999).fill(false);
+
+  const results: ImageStats = {
+    isContinuous: true,
+    numFiles: 0,
+    numMissing: 0,
+    earliestFile: "",
+    lastFile: "",
+    formatType: -1,
+    smallestIndex: 999999,
+    largestIndex: 0,
+  };
+  let ed = new Date();
+  let ld = new Date(0);
+
+  try {
+    const files = fs.readdirSync(directoryPath);
+    files.forEach((file: string) => {
+      results.numFiles++;
+      if (results.formatType === -1) {
+        if (file.match(/image\d{4}.jpg/)) {
+          results.formatType = 0;
+        } else if (file.match(/image\d{5}.jpg/)) {
+          results.formatType = 1;
+        } else if (file.match(/image-\d-\d{5}.jpg/)) {
+          results.formatType = 2;
+        }
+      }
+      const fileDate = getFileDate(directoryPath + "/" + file);
+      if (fileDate) {
+        if (fileDate < ed) {
+          ed = fileDate;
+          results.earliestFile = file;
+        }
+        if (fileDate > ld) {
+          ld = fileDate;
+          results.lastFile = file;
+        }
+      }
+      const filePath = `${directoryPath}/${file}`;
+      //extract the 4 or 5 digit number from the name
+      let num = parseInt(file.match(/\d{4,5}/)![0]);
+      if (results.formatType == 0) num -= 1000;
+      else num -= 10000;
+      index[num] = true;
+      if (num > results.largestIndex) results.largestIndex = num;
+      if (num < results.smallestIndex) results.smallestIndex = num;
+    });
+    for (let i = results.smallestIndex; i <= results.largestIndex; i++) {
+      if (!index[i]) {
+        results.isContinuous = false;
+        results.numMissing++;
+      }
+    }
+    results.earliestTime = ed.getTime();
+    results.lastTime = ld.getTime();
+    return results;
+  } catch (err: any) {
+    console.error(err);
+    results.error = err.message;
+    return results;
+  }
+}
+
+//scan all files and create teh information database
 const go = async () => {
   let results: any = { images: {}, videos: {} };
   //scan the /app/gliderport directory for directories of the form 20xx where xx are numbers
@@ -72,24 +171,25 @@ const go = async () => {
           let days = fs.readdirSync(`/app/gliderport/${year}/${month}`);
           for (let k = 0; k < days.length; k++) {
             let day = days[k];
-            // scan that directory for year-month-day format directories (two numbers) that are directories themselves
-            //   if (year == "2021" && month == "08" && day == "2021-08-10") {
-            results.images[year][month][day] = getImageStats(`/app/gliderport/${year}/${month}/${day}`);
-            console.log("results: ", JSON.stringify(results.images[year][month][day]));
-            //   } else results.images[year][month][day] = {};
+            if (day.match(/^image.*$/)) {
+              results.images[year][month][day] = getImageStats(`/app/gliderport/${year}/${month}/${day}`);
+              results.images[year][month][day].video = fs
+                .readdirSync(`/app/gliderport/video/${year}/`)
+                .filter((fn) => fn.match(`/^${year}-${month}-${day}.*mp4$/`));
+              console.log("results: ", JSON.stringify(results.images[year][month][day]));
+            }
           }
           const id = ToId(year + month);
           console.log("id: ", id);
           await pb
             .collection("ImageFileData")
-            .create({ id, data: results.images[year][month] })
+            .update(id, { data: results.images[year][month] })
             .catch((err: any) => console.error(err.message));
         }
       }
     }
   }
 };
-
 await go();
 
 dotenv.config();
@@ -449,101 +549,3 @@ app.get("/PhoneFinder", (req, res) => {
     res.send("none");
   }
 });
-
-function isDirectory(path: string): boolean {
-  try {
-    return fs.statSync(path).isDirectory();
-  } catch (err) {
-    console.error(err);
-    return false;
-  }
-}
-
-function getFileDate(filePath: string): Date | null {
-  try {
-    const stats = fs.statSync(filePath);
-    return stats.mtime;
-  } catch (err) {
-    console.error(err);
-    return null;
-  }
-}
-
-interface ImageStats {
-  earliestFile: string;
-  earliestTime?: number;
-  isContinuous: boolean;
-  formatType: number; // 0:image1000.jpg 1:image10000.jpg 2:image-1/2-10000.jpg
-  lastFile: string;
-  lastTime?: number;
-  numFiles: number;
-  numMissing: number;
-  error?: string;
-  smallestIndex: number;
-  largestIndex: number;
-}
-
-function getImageStats(directoryPath: string): ImageStats {
-  let index: boolean[] = Array(9999).fill(false);
-
-  const results: ImageStats = {
-    isContinuous: true,
-    numFiles: 0,
-    numMissing: 0,
-    earliestFile: "",
-    lastFile: "",
-    formatType: -1,
-    smallestIndex: 999999,
-    largestIndex: 0,
-  };
-  let ed = new Date();
-  let ld = new Date(0);
-
-  try {
-    const files = fs.readdirSync(directoryPath);
-    files.forEach((file: string) => {
-      results.numFiles++;
-      if (results.formatType === -1) {
-        if (file.match(/image\d{4}.jpg/)) {
-          results.formatType = 0;
-        } else if (file.match(/image\d{5}.jpg/)) {
-          results.formatType = 1;
-        } else if (file.match(/image-\d-\d{5}.jpg/)) {
-          results.formatType = 2;
-        }
-      }
-      const fileDate = getFileDate(directoryPath + "/" + file);
-      if (fileDate) {
-        if (fileDate < ed) {
-          ed = fileDate;
-          results.earliestFile = file;
-        }
-        if (fileDate > ld) {
-          ld = fileDate;
-          results.lastFile = file;
-        }
-      }
-      const filePath = `${directoryPath}/${file}`;
-      //extract the 4 or 5 digit number from the name
-      let num = parseInt(file.match(/\d{4,5}/)![0]);
-      if (results.formatType == 0) num -= 1000;
-      else num -= 10000;
-      index[num] = true;
-      if (num > results.largestIndex) results.largestIndex = num;
-      if (num < results.smallestIndex) results.smallestIndex = num;
-    });
-    for (let i = results.smallestIndex; i <= results.largestIndex; i++) {
-      if (!index[i]) {
-        results.isContinuous = false;
-        results.numMissing++;
-      }
-    }
-    results.earliestTime = ed.getTime();
-    results.lastTime = ld.getTime();
-    return results;
-  } catch (err: any) {
-    console.error(err);
-    results.error = err.message;
-    return results;
-  }
-}
