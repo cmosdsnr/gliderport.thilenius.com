1391654c2b5b106dd2b3cd3439be104d229eac70
Stephen <stephen@thilenius.com>
2025-06-04 10:17:37 -0700

    wip

--- src/log.ts
+++ src/log.ts
@@ -17,7 +17,7 @@ import { isDirectory } from "miscellaneous.js";
 // Determine the logs directory based on the existence of the "/app/gliderport/logs" folder.
 const logsDir = isDirectory("/app/gliderport/logs") ? "/app/gliderport/logs/" : "./public/logs/";
 // Define the log file path.
-let __logFile = logsDir + `gpUpdate.log`;
+let __logFile = "/app/gliderport/logs/" + `gpUpdate.log`;
 
 /**
  * Logs a message to a file.
